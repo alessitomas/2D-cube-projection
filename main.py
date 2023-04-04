b21ada5576c45fac92996e5e99d9851640595f07
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2023-04-04 16:37:26 -0300

    adicionando comentários

--- main.py
+++ main.py
@@ -24,46 +24,50 @@ rotacao_z = np.array([[np.cos(angulo), -np.sin(angulo), 0, 0], [np.sin(angulo), 
 rotacao_total = rotacao_x @ rotacao_y @ rotacao_z
 
 # Matrizes de translação no Z
-translacao_z1 = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, d], [0, 0, 0, 1]])
+translacao_z = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, d], [0, 0, 0, 1]])
 translacao_centro = np.array([[1, 0, 0, 400], [0, 1, 0, 300], [0, 0, 1, 0], [0, 0, 0, 1]])
 
 # Pinhole
 m_pinhole = np.array([[1,0,0,0],[0,1,0,0],[0,0,0,-d],[0,0,-(1/d),0]])
 
+# Loop principal
 rodando = True
 while rodando:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             rodando = False
 
+    # Diminuindo a velocidade das transformações
     pygame.time.delay(10)
 
+    # Atualizando a rotação
     rotacao_total = rotacao_total @ rotacao_y @ rotacao_z @ rotacao_x
 
-    M = translacao_centro @ m_pinhole @ translacao_z1 @ rotacao_total
-    proj = M @ cubo
+    # Juntando todas as transformações em uma matriz só chamada M
+    M = translacao_centro @ m_pinhole @ translacao_z @ rotacao_total
 
+    # Multiplicando a matriz M pelo cubo, gerando um novo cubo com as transformações
+    final = M @ cubo
+
+    # Preencher a tela com preto para não deixar rastro
     screen.fill((0, 0, 0))
 
-    # Criar linhas
-    pygame.draw.line(screen, (255, 0, 0), (proj[0, 0]/proj[3, 0], proj[1, 0]/proj[3, 0]), (proj[0, 1]/proj[3, 1], proj[1, 1]/proj[3, 1]), 5)
-    pygame.draw.line(screen, (255, 0, 0), (proj[0, 1]/proj[3, 1], proj[1, 1]/proj[3, 1]), (proj[0, 2]/proj[3, 2], proj[1, 2]/proj[3, 2]), 5)
-    pygame.draw.line(screen, (255, 0, 0), (proj[0, 2]/proj[3, 2], proj[1, 2]/proj[3, 2]), (proj[0, 3]/proj[3, 3], proj[1, 3]/proj[3, 3]), 5)
-    pygame.draw.line(screen, (255, 0, 0), (proj[0, 3]/proj[3, 3], proj[1, 3]/proj[3, 3]), (proj[0, 0]/proj[3, 0], proj[1, 0]/proj[3, 0]), 5)
+    # Criar linhas que ligam os pontos do cubo -> Transforma o XpWp em Xp e o YpWp em Yp
+    pygame.draw.line(screen, (255, 0, 0), (final[0, 0]/final[3, 0], final[1, 0]/final[3, 0]), (final[0, 1]/final[3, 1], final[1, 1]/final[3, 1]), 3)
+    pygame.draw.line(screen, (255, 0, 0), (final[0, 1]/final[3, 1], final[1, 1]/final[3, 1]), (final[0, 2]/final[3, 2], final[1, 2]/final[3, 2]), 3)
+    pygame.draw.line(screen, (255, 0, 0), (final[0, 2]/final[3, 2], final[1, 2]/final[3, 2]), (final[0, 3]/final[3, 3], final[1, 3]/final[3, 3]), 3)
+    pygame.draw.line(screen, (255, 0, 0), (final[0, 3]/final[3, 3], final[1, 3]/final[3, 3]), (final[0, 0]/final[3, 0], final[1, 0]/final[3, 0]), 3)
+    pygame.draw.line(screen, (255, 0, 0), (final[0, 4]/final[3, 4], final[1, 4]/final[3, 4]), (final[0, 5]/final[3, 5], final[1, 5]/final[3, 5]), 3)
+    pygame.draw.line(screen, (255, 0, 0), (final[0, 5]/final[3, 5], final[1, 5]/final[3, 5]), (final[0, 6]/final[3, 6], final[1, 6]/final[3, 6]), 3)
 
-    pygame.draw.line(screen, (255, 0, 0), (proj[0, 4]/proj[3, 4], proj[1, 4]/proj[3, 4]), (proj[0, 5]/proj[3, 5], proj[1, 5]/proj[3, 5]), 5)
-    pygame.draw.line(screen, (255, 0, 0), (proj[0, 5]/proj[3, 5], proj[1, 5]/proj[3, 5]), (proj[0, 6]/proj[3, 6], proj[1, 6]/proj[3, 6]), 5)
-    pygame.draw.line(screen, (255, 0, 0), (proj[0, 6]/proj[3, 6], proj[1, 6]/proj[3, 6]), (proj[0, 7]/proj[3, 7], proj[1, 7]/proj[3, 7]), 5)
-    pygame.draw.line(screen, (255, 0, 0), (proj[0, 7]/proj[3, 7], proj[1, 7]/proj[3, 7]), (proj[0, 4]/proj[3, 4], proj[1, 4]/proj[3, 4]), 5)
-
-    pygame.draw.line(screen, (255, 0, 0), (proj[0, 0]/proj[3, 0], proj[1, 0]/proj[3, 0]), (proj[0, 4]/proj[3, 4], proj[1, 4]/proj[3, 4]), 5)
-    pygame.draw.line(screen, (255, 0, 0), (proj[0, 1]/proj[3, 1], proj[1, 1]/proj[3, 1]), (proj[0, 5]/proj[3, 5], proj[1, 5]/proj[3, 5]), 5)
-    pygame.draw.line(screen, (255, 0, 0), (proj[0, 2]/proj[3, 2], proj[1, 2]/proj[3, 2]), (proj[0, 6]/proj[3, 6], proj[1, 6]/proj[3, 6]), 5)
-    pygame.draw.line(screen, (255, 0, 0), (proj[0, 3]/proj[3, 3], proj[1, 3]/proj[3, 3]), (proj[0, 7]/proj[3, 7], proj[1, 7]/proj[3, 7]), 5)
+    pygame.draw.line(screen, (255, 0, 0), (final[0, 6]/final[3, 6], final[1, 6]/final[3, 6]), (final[0, 7]/final[3, 7], final[1, 7]/final[3, 7]), 3)
+    pygame.draw.line(screen, (255, 0, 0), (final[0, 7]/final[3, 7], final[1, 7]/final[3, 7]), (final[0, 4]/final[3, 4], final[1, 4]/final[3, 4]), 3)
+    pygame.draw.line(screen, (255, 0, 0), (final[0, 0]/final[3, 0], final[1, 0]/final[3, 0]), (final[0, 4]/final[3, 4], final[1, 4]/final[3, 4]), 3)
+    pygame.draw.line(screen, (255, 0, 0), (final[0, 1]/final[3, 1], final[1, 1]/final[3, 1]), (final[0, 5]/final[3, 5], final[1, 5]/final[3, 5]), 3)
+    pygame.draw.line(screen, (255, 0, 0), (final[0, 2]/final[3, 2], final[1, 2]/final[3, 2]), (final[0, 6]/final[3, 6], final[1, 6]/final[3, 6]), 3)
+    pygame.draw.line(screen, (255, 0, 0), (final[0, 3]/final[3, 3], final[1, 3]/final[3, 3]), (final[0, 7]/final[3, 7], final[1, 7]/final[3, 7]), 3)
     
     # Atualiza a tela
-    pygame.display.flip()
-
     pygame.display.update()
 
 pygame.quit()
